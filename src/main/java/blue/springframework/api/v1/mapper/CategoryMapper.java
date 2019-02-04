07a5401549af40ed9aa9fa5e17cc7132f634a2c1
CyrilenBlu <lukepetzerint2@gmail.com>
2019-02-04 12:09:22 +0200

    Added Mapping id.

--- src/main/java/blue/springframework/api/v1/mapper/CategoryMapper.java
+++ src/main/java/blue/springframework/api/v1/mapper/CategoryMapper.java
@@ -3,12 +3,13 @@ package blue.springframework.api.v1.mapper;
 import blue.springframework.api.v1.model.CategoryDTO;
 import blue.springframework.domain.Category;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface CategoryMapper {
     CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
 
-    // @Mapping(source = "getId", target = "id")
+    @Mapping(source = "id", target = "id")
     CategoryDTO categoryToCategoryDTO(Category category);
 }
