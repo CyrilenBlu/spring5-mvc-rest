5eb1def4e0fb0450fc06dd1383c2f58037806715
CyrilenBlu <lukepetzerint2@gmail.com>
2019-02-04 13:11:23 +0200

    Added CategoryService.

---
+++ src/test/java/blue/springframework/services/CategoryService.java
@@ -0,0 +1,12 @@
+package blue.springframework.services;
+
+import blue.springframework.api.v1.model.CategoryDTO;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    List<CategoryDTO> getAllCategories();
+
+    CategoryDTO getCategoryByName(String name);
+}
